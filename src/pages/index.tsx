0a75ffcd52c663760ee05fff95c2d87e0177faec
Syncxv <aruldevarackal1@gmail.com>
2021-11-01 03:41:03 +0530

    init

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.scss'
+
+const Home: NextPage = () => {
+  return (
+    <>
+    <div className="">well</div>
+    </>
+  )
+}
+
+export default Home
