0cac5d775d3fb7119b89861eaa5d39b09d94373e
Syncxv <aruldevarackal1@gmail.com>
2021-11-01 03:46:57 +0530

    GOT tailwind working :D

---
+++ src/pages/output.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from 'next'
+
+const output: NextPage  = () => {
+    return (
+        <div>
+            <div className="bg-gray-50 h-52 w-52">wel</div>
+        </div>
+    )
+}
+
+export default output
